747d8a9cc829320fef1f43a48ea05f1e6a714100
NatalyGraham <natalygraham2606@gmail.com>
2022-02-23 15:19:36 +0200

    hw01_UPD

--- js/search.js
+++ js/search.js
@@ -1,10 +1,15 @@
 const search = () => {
 	const input = document.querySelector('.search-block > input');
+	console.dir(input);
+	
 	const searchBtn = document.querySelector('.search-block > button');
 
-	input.addEventListener('click', (event) => {
-		console.log(event.target.value);
-		
+	let inputText;
+	input.addEventListener('input', (event) => {
+		inputText = event.target.value;
+	});
+	searchBtn.addEventListener('click', () => {
+		console.log(inputText);		
 	})
 };
 
